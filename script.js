04bf086add93b3c2ae976108a02794775805f75b
arpitgupta854 <arpitguptajalaun@gmail.com>
2023-08-27 23:17:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,94 @@
+function myfunction(){
+var data=document.getElementById("demo");
+var sign=document.getElementById("plus");
+console.log(sign);
+    if(data.style.display=="none"){
+        data.style.display="block";
+        sign.innerHTML="✖";
+    }
+   else{
+    data.style.display="none";
+    sign.innerHTML="+";
+   }
+}
+
+function myfunction1(){
+    var data=document.getElementById("demo1");
+    var sign=document.getElementById("plus1");
+    if(data.style.display=="none"){
+        data.style.display="block";
+        sign.innerHTML="✖";
+    }
+   else{
+    data.style.display="none";
+    sign.innerHTML="+";
+   } 
+}
+
+// function myfunction2(){
+//     var data=document.getElementById("demo2");
+//     var sign=document.getElementById("plus3");
+//     if(data.style.display=="none"){
+//         data.style.display="block";
+//         sign.innerHTML="✖";
+//     }
+//    else{
+//     data.style.display="none";
+//     sign.innerHTML="+";
+//    }  
+// }
+
+function myfunction3(){
+    var data=document.getElementById("demo3");
+    var sign=document.getElementById("plus3");
+    if(data.style.display=="none"){
+        data.style.display="block";
+        sign.innerHTML="✖";
+    }
+   else{
+    data.style.display="none";
+    sign.innerHTML="+";
+   }     
+}
+
+function myfunction4(){
+    var data=document.getElementById("demo4");
+    var sign=document.getElementById("plus4");
+    if(data.style.display=="none"){
+        data.style.display="block";
+        sign.innerHTML="✖";
+    }
+   else{
+    data.style.display="none";
+    sign.innerHTML="+";
+   }     
+}
+
+function myfunction8(){
+    var data=document.getElementById("demo8");
+    var sign=document.getElementById("plus5");
+    if(data.style.display=="none"){
+        data.style.display="block";
+        sign.innerHTML="✖";
+    }
+   else{
+    data.style.display="none";
+    sign.innerHTML="+";
+   } 
+}
+
+function myfunction10(){
+    var data=document.getElementById("demo10");
+    var sign=document.getElementById("plus2");
+    if(data.style.display=="none"){
+        data.style.display="block";
+        sign.innerHTML="✖";
+    }
+   else{
+    data.style.display="none";
+    sign.innerHTML="+";
+   } 
+}
+
+
+
